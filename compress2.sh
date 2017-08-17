c27fe5060bc5f4bd1faf16cfecf76c7d77a731de
William Joye <wjoye@cfa.harvard.edu>
2017-08-17 15:52:12 -0400

    new tests

---
+++ compress2.sh
@@ -0,0 +1,12 @@
+echo
+echo "*** COMPRESS2 ***"
+
+for f in compress2/* 
+do 
+    echo " ${f#compress2/}"
+    rm -f foo.fits
+    funpack -O foo.fits $f
+    ds9 $f foo.fits -mode crosshair -lock crosshair image -lock scale -lock colorbar -lock frame image -lock slice
+done
+
+echo "PASSED"
