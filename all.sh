e932c6d606c21e790a1409c20b985b209826ea20
William Joye <wjoye@cfa.harvard.edu>
2018-02-12 17:05:21 -0500

    update tests

--- all.sh
+++ all.sh
@@ -4,24 +4,28 @@
 #command.sh
 #samp.sh
 
-parse.sh
-
-crop.sh
 regions.sh
-ciaoregs.sh
 regions3d.sh
+ciaoregs.sh
+
+parse.sh
+crop.sh
+
 plot.sh
 scale.sh
 lock.sh
 prefs.sh
 matrix.sh
 align.sh
+
 wcs.sh
 wcs2.sh
+
 cat.sh
 sia.sh
 #vo.sh xpa
 vo.sh mime
+
 print.sh
 backup.sh
 analysis.sh
