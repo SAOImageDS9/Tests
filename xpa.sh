5af57bfc89c1fcc62538d2e09c250067bbda8560
William Joye <wjoye@cfa.harvard.edu>
2018-03-08 12:50:10 -0500

    update tests

--- xpa.sh
+++ xpa.sh
@@ -81,7 +81,8 @@ xpaset -p ds9 2mass m51
 xpaset -p ds9 2mass name m51
 xpaget ds9 2mass name >> ${tt}.out
 xpaset -p ds9 2mass name {}
-xpaset -p ds9 2mass coord 00:42:44.404 +41:16:08.78 sexagesimal
+xpaset -p ds9 2mass coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 2mass 13:29:52.37 +47:11:40.8
 xpaget ds9 2mass coord >> ${tt}.out
 xpaset -p ds9 2mass update frame
 xpaset -p ds9 mode crosshair
@@ -680,7 +681,8 @@ xpaset -p ds9 dsssao m51
 xpaset -p ds9 dsssao name m51
 xpaget ds9 dsssao name >> ${tt}.out
 xpaset -p ds9 dsssao name {}
-xpaset -p ds9 dsssao coord 00:42:44.404 +41:16:08.78 sexagesimal
+xpaset -p ds9 dsssao coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 dsssao 13:29:52.37 +47:11:40.8
 xpaget ds9 dsssao coord >> ${tt}.out
 xpaset -p ds9 dsssao update frame
 xpaset -p ds9 mode crosshair
@@ -715,7 +717,8 @@ xpaset -p ds9 dsseso m51
 xpaset -p ds9 dsseso name m51
 xpaget ds9 dsseso name >> ${tt}.out
 xpaset -p ds9 dsseso name {}
-xpaset -p ds9 dsseso coord 00:42:44.404 +41:16:08.78 sexagesimal
+xpaset -p ds9 dsseso coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 dsseso 13:29:52.37 +47:11:40.8
 xpaget ds9 dsseso coord >> ${tt}.out
 xpaset -p ds9 dsseso update frame
 xpaset -p ds9 mode crosshair
@@ -750,7 +753,8 @@ xpaset -p ds9 dssstsci m51
 xpaset -p ds9 dssstsci name m51
 xpaget ds9 dssstsci name >> ${tt}.out
 xpaset -p ds9 dssstsci name {}
-xpaset -p ds9 dssstsci coord 00:42:44.404 +41:16:08.78 sexagesimal
+xpaset -p ds9 dssstsci coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 dssstsci 13:29:52.37 +47:11:40.8
 xpaget ds9 dssstsci coord >> ${tt}.out
 xpaset -p ds9 dssstsci update frame
 xpaset -p ds9 mode crosshair
@@ -1863,6 +1867,7 @@ xpaset -p ds9 nvss name m51
 xpaget ds9 nvss name >> ${tt}.out
 xpaset -p ds9 nvss name {}
 xpaset -p ds9 nvss coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 nvss 13:29:52.37 +47:11:40.8
 xpaget ds9 nvss coord >> ${tt}.out
 xpaset -p ds9 nvss update frame
 xpaset -p ds9 mode crosshair
@@ -2928,7 +2933,8 @@ xpaset -p ds9 skyview m51
 xpaset -p ds9 skyview name m51
 xpaget ds9 skyview name >> ${tt}.out
 xpaset -p ds9 skyview name {}
-xpaset -p ds9 skyview coord 00:42:44.404 +41:16:08.78 sexagesimal
+xpaset -p ds9 skyview coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 skyview 13:29:52.37 +47:11:40.8
 xpaget ds9 skyview coord >> ${tt}.out
 xpaset -p ds9 skyview update frame
 xpaset -p ds9 mode crosshair
@@ -3226,6 +3232,7 @@ xpaset -p ds9 vla name m51
 xpaget ds9 vla name >> ${tt}.out
 xpaset -p ds9 vla name {}
 xpaset -p ds9 vla coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 vla 13:29:52.37 +47:11:40.8
 xpaget ds9 vla coord >> ${tt}.out
 xpaset -p ds9 vla update frame
 xpaset -p ds9 mode crosshair
@@ -3259,6 +3266,7 @@ xpaset -p ds9 vlss name m51
 xpaget ds9 vlss name >> ${tt}.out
 xpaset -p ds9 vlss name {}
 xpaset -p ds9 vlss coord 13:29:52.37 +47:11:40.8 sexagesimal
+xpaset -p ds9 vlss 13:29:52.37 +47:11:40.8
 xpaget ds9 vlss coord >> ${tt}.out
 xpaset -p ds9 vlss update frame
 xpaset -p ds9 mode crosshair
