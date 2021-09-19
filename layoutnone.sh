9be915b351b28c1747b2db25880bd293266e2278
William Joye <wjoye@cfa.harvard.edu>
2021-09-19 12:13:35 -0400

    update tests

--- layoutnone.sh
+++ layoutnone.sh
@@ -4,123 +4,277 @@ echo "Starting..."
 
 tt="none"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -view colorbar no&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete &
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -colorbar vertical&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbhgrh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -view graph horizontal yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -view graph vertical yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbhgrhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -view graph horizontal yes -view graph vertical yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbvgrh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -colorbar vertical -view graph horizontal yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbvgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -colorbar vertical  -view graph vertical yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="cbvgrhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -colorbar vertical -view graph horizontal yes -view graph vertical yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="grh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -view colorbar no -view graph horizontal yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="grv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -view colorbar no -view graph vertical yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
 
 tt="grhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
-echo "$tt..."
+echo -n "$tt..."
 eval "$org -title foo -restore layout/none/$tt.bck&"
 ds9 -title bar -frame delete -view colorbar no -view graph horizontal yes -view graph vertical yes&
 read
+echo -n "  numerics no"
+xpaset -p bar colorbar numerics no
+read
+echo -n "  numerics yes"
+xpaset -p bar colorbar numerics yes
+read
+echo -n "  fontsize 36"
+xpaset -p bar colorbar fontsize 36
+read
+echo -n "  fontsize 9"
+xpaset -p bar colorbar fontsize 9
+read
+echo "...done"
 xpaset -p foo exit
 xpaset -p bar exit
 fi
-
-echo "Done"
 
