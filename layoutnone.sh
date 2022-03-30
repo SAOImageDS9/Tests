c32ae8566a204971e7799e959b0dd90f8eaf2dff
William Joye <wjoye@cfa.harvard.edu>
2022-03-30 13:39:04 -0400

    update tests

--- layoutnone.sh
+++ layoutnone.sh
@@ -1,22 +1,19 @@
-org='~/Downloads/ds9.darwinbigsurarm64.8.3b1/ds9'
-
 testit () {
     read
     echo -n "  numerics no"
-    xpaset -p bar colorbar numerics no
+    xpaset -p ds9 colorbar numerics no
     read
     echo -n "  numerics yes"
-    xpaset -p bar colorbar numerics yes
+    xpaset -p ds9 colorbar numerics yes
     read
     echo -n "  fontsize 36"
-    xpaset -p bar colorbar fontsize 36
+    xpaset -p ds9 colorbar fontsize 36
     read
     echo -n "  fontsize 9"
-    xpaset -p bar colorbar fontsize 9
+    xpaset -p ds9 colorbar fontsize 9
     read
     echo "...done"
-    xpaset -p foo exit
-    xpaset -p bar exit
+    xpaset -p ds9 exit
 }
 
 echo "Starting..."
@@ -24,96 +21,84 @@ echo "Starting..."
 tt="none"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -view colorbar no&
+ds9 -frame delete -view colorbar no&
 testit
 fi
 
 tt="cbh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete &
+ds9 -frame delete &
 testit
 fi
 
 tt="cbv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -colorbar vertical&
+ds9 -frame delete -colorbar vertical&
 testit
 fi
 
 tt="cbhgrh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -view graph horizontal yes&
+ds9 -frame delete -view graph horizontal yes&
 testit
 fi
 
 tt="cbhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -view graph vertical yes&
+ds9 -frame delete -view graph vertical yes&
 testit
 fi
 
 tt="cbhgrhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -view graph horizontal yes -view graph vertical yes&
+ds9 -frame delete -view graph horizontal yes -view graph vertical yes&
 testit
 fi
 
 tt="cbvgrh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -colorbar vertical -view graph horizontal yes&
+ds9 -frame delete -colorbar vertical -view graph horizontal yes&
 testit
 fi
 
 tt="cbvgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -colorbar vertical  -view graph vertical yes&
+ds9 -frame delete -colorbar vertical  -view graph vertical yes&
 testit
 fi
 
 tt="cbvgrhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -colorbar vertical -view graph horizontal yes -view graph vertical yes&
+ds9 -frame delete -colorbar vertical -view graph horizontal yes -view graph vertical yes&
 testit
 fi
 
 tt="grh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -view colorbar no -view graph horizontal yes&
+ds9 -frame delete -view colorbar no -view graph horizontal yes&
 testit
 fi
 
 tt="grv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -view colorbar no -view graph vertical yes&
+ds9 -frame delete -view colorbar no -view graph vertical yes&
 testit
 fi
 
 tt="grhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/none/$tt.bck&"
-ds9 -title bar -frame delete -view colorbar no -view graph horizontal yes -view graph vertical yes&
+ds9 -frame delete -view colorbar no -view graph horizontal yes -view graph vertical yes&
 testit
 fi
 
--- layoutone.sh
+++ layoutone.sh
@@ -1,22 +1,19 @@
-org='~/Downloads/ds9.darwinbigsurarm64.8.3b1/ds9'
-
 testit () {
     read
     echo -n "  numerics no"
-    xpaset -p bar colorbar numerics no
+    xpaset -p ds9 colorbar numerics no
     read
     echo -n "  numerics yes"
-    xpaset -p bar colorbar numerics yes
+    xpaset -p ds9 colorbar numerics yes
     read
     echo -n "  fontsize 36"
-    xpaset -p bar colorbar fontsize 36
+    xpaset -p ds9 colorbar fontsize 36
     read
     echo -n "  fontsize 9"
-    xpaset -p bar colorbar fontsize 9
+    xpaset -p ds9 colorbar fontsize 9
     read
     echo "...done"
-    xpaset -p foo exit
-    xpaset -p bar exit
+    xpaset -p ds9 exit
 }
 
 echo "Starting..."
@@ -24,96 +21,84 @@ echo "Starting..."
 tt="none"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -view colorbar no&
+ds9 fits/img.fits -view colorbar no&
 testit
 fi
 
 tt="cbh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits&
+ds9 fits/img.fits&
 testit
 fi
 
 tt="cbv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -colorbar vertical&
+ds9 fits/img.fits -colorbar vertical&
 testit
 fi
 
 tt="cbhgrh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -view graph horizontal yes&
+ds9 fits/img.fits -view graph horizontal yes&
 testit
 fi
 
 tt="cbhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -view graph vertical yes&
+ds9 fits/img.fits -view graph vertical yes&
 testit
 fi
 
 tt="cbhgrhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -view graph horizontal yes -view graph vertical yes&
+ds9 fits/img.fits -view graph horizontal yes -view graph vertical yes&
 testit
 fi
 
 tt="cbvgrh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -colorbar vertical -view graph horizontal yes&
+ds9 fits/img.fits -colorbar vertical -view graph horizontal yes&
 testit
 fi
 
 tt="cbvgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -colorbar vertical  -view graph vertical yes&
+ds9 fits/img.fits -colorbar vertical  -view graph vertical yes&
 testit
 fi
 
 tt="cbvgrhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -colorbar vertical -view graph horizontal yes -view graph vertical yes&
+ds9 fits/img.fits -colorbar vertical -view graph horizontal yes -view graph vertical yes&
 testit
 fi
 
 tt="grh"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -view colorbar no -view graph horizontal yes&
+ds9 fits/img.fits -view colorbar no -view graph horizontal yes&
 testit
 fi
 
 tt="grv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -view colorbar no -view graph vertical yes&
+ds9 fits/img.fits -view colorbar no -view graph vertical yes&
 testit
 fi
 
 tt="grhgrv"
 if [ "$1" = "$tt" -o -z "$1" ]; then
 echo -n "$tt..."
-eval "$org -title foo -restore layout/one/$tt.bck&"
-ds9 -title bar fits/img.fits -view colorbar no -view graph horizontal yes -view graph vertical yes&
+ds9 fits/img.fits -view colorbar no -view graph horizontal yes -view graph vertical yes&
 testit
 fi
 
